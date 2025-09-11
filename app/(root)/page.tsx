a7777cf5a39e29ab3c5e845eb59992a69465501e
Luis Martinez <Luisricardo494@gmail.com>
2025-09-11 16:22:58 -0500

    Add header, searchbar, and card (#1)
    Co-authored-by: Luis <beebs@Luiss-MacBook-Pro.local>

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,53 @@
+import SearchForm from "../components/SearchForm/SearchForm";
+import StartupCard from "../components/StartupCard";
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams?: { query?: string };
+}) {
+  const query = (await searchParams)?.query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Keanu" },
+      _id: 1,
+      description: "This is a descrption",
+      imageSmall: "../public/touch_640.jpg",
+      imageMedium: "../public/touch_1280.jpg",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
+
   return (
-    <div>
-      <h1 className="text-2xl">Home</h1>
-    </div>
+    <>
+      <section className="pink_container">
+        <h1 className="heading max-w-3xl ">
+          Its time to learn, <br /> Feel the burn!
+        </h1>
+        <p className="sub-heading max-w-2xl">
+          "I want to make a good, solid kung fu movie." - Keanu Reeves
+        </p>
+        <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All sections"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No sections found</p>
+          )}
+        </ul>
+      </section>
+    </>
   );
 }
---
+++ app/utils/datePicker.ts
@@ -0,0 +1,8 @@
+export function formatDate(d: string | Date) {
+  const date = typeof d === "string" ? new Date(d) : d;
+  return date.toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
+}
