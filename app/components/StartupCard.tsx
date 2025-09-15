cdfc53b6039a4e1b2028354c0802f95657436004
Luis <beebs@Luiss-MacBook-Pro.local>
2025-09-15 12:46:20 -0500

    added a card

--- app/components/StartupCard.tsx
+++ app/components/StartupCard.tsx
@@ -3,6 +3,7 @@ import { formatDate } from "../utils/datePicker";
 import { EyeIcon } from "lucide-react";
 import { StartupTypeCard } from "@/types/startup";
 import Link from "next/link";
+import Image from "next/image";
 
 const StartupCard = ({
   post: {
@@ -10,6 +11,10 @@ const StartupCard = ({
     views,
     _id,
     title,
+    description,
+    imageSmall,
+    imageMedium,
+    category,
     author: { _id: authorId, name },
   },
 }: {
@@ -26,13 +31,37 @@ const StartupCard = ({
       </div>
       <div className="flex-between mt-5 gap-5">
         <div className="flex-1">
-          <Link href={`/user/${authorId}`}>
-            <p className="text-16-medium line-clamp-1">{name}</p>
-          </Link>
-          <Link href={`/startp/${_id}`}>
-            <h3 className="text-26-semibold line-clamp-1">{title}</h3>
-          </Link>
+          <p className="text-16-medium line-clamp-1">
+            <Link href={`/user/${authorId}`}>{name}</Link>
+          </p>
+          <h3 className="text-26-semibold line-clamp-1">
+            <Link href={`/startp/${_id}`}>{title}</Link>
+          </h3>
         </div>
+        <Link href={`/user/${authorId}`}>
+          <Image
+            src="/touch_1280.jpg"
+            alt="placeholder"
+            width={48}
+            height={48}
+            className="rounded-full"
+          />
+        </Link>
+      </div>
+
+      <Link href={`/startup/${_id}`}>
+        <p className="startup-card_desc">{description}</p>
+        {/* <Image src="/touch_1280.jpg" alt="placeholder" className="startup-card_img"/> */}
+        <img src={imageSmall} alt="placeholder" className="startup-card_img" />
+      </Link>
+
+      <div className="flex-between gap-3 mt-5">
+        <Link href={`/?query=${category.toLowerCase()}`}>
+          <p className="text-16-medium">{category}</p>
+        </Link>
+        <button className="startup-card_btn">
+          <Link href={`/startup/${_id}`}>Details</Link>
+        </button>
       </div>
     </li>
   );
